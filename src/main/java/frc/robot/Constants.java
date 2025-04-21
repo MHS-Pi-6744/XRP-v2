458c8dcdf87a7d019a9db07236c6b586445fa066
MitchRM <mitchrm2@gmail.com>
2025-04-21 11:21:23 -0500

    Move drivetrain constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,19 +1,19 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// XRP Project Constants
 
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
+
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+  }
+
+  public static class DrivetrainConstants {
+    private static final double kGearRatio =
+      (30.0 / 14.0) * (28.0 / 16.0) * (36.0 / 9.0) * (26.0 / 8.0); // Total gear ratio is 48.75 motor rotations to 1 wheel revolution
+    private static final double kCountsPerMotorShaftRev = 12.0; // Encoder(?) counts per motor shaft revolution
+    private static final double kCountsPerRevolution = kCountsPerMotorShaftRev * kGearRatio; // 585.0 counts per wheel revolution
+    private static final double kWheelDiameterInch = 2.3622; // 60 mm
   }
 }
